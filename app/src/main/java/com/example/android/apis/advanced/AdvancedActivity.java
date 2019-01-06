14e247591d247ca19876b982a12870b0dccc15b0
WenpingWang <wenping0820@163.com>
2019-01-06 15:41:24 +0800

    [dev] Use third-party library CodeView to show the code content
    1. CodeView can hightlight code;
       You can get more information about the libaray in
       https://github.com/Thereisnospon/CodeView;
    
    Next step: Show method list and jump to method by clicking it;

--- app/src/main/java/com/example/android/apis/advanced/AdvancedActivity.java
+++ app/src/main/java/com/example/android/apis/advanced/AdvancedActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.example.android.apis.R;
 
@@ -18,20 +17,38 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
+import thereisnospon.codeview.CodeView;
+
 /**
  * Advanced activity.
+ * <p>
+ * Features:
+ * 1. Search file according to current activity;
+ * 2. Show file content with {@link CodeView} with can highlight the code;
+ * Get more information about the library in {@link https://github.com/Thereisnospon/CodeView};
+ * </p>
  */
 public class AdvancedActivity extends Activity {
-    /** Extra data for current activity name. */
+    /**
+     * Extra data for current activity name.
+     */
     public static final String EXTRA_CUR_ACTIVITY_NAME = "cur_activity_name";
-    /** Log tag. */
+    /**
+     * Log tag.
+     */
     private static final String TAG = AdvancedActivity.class.getSimpleName();
-    /** Request code for write external storage permission. */
+    /**
+     * Request code for write external storage permission.
+     */
     private static final int REQ_CODE_WRITE_STORAGE_PERMISSION = 0;
-    /** Current activity name. */
+    /**
+     * Current activity name.
+     */
     private String mCurActivityName;
-    /** Code text view. */
-    private TextView mCodeView;
+    /**
+     * Code view.
+     */
+    private CodeView mCodeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +56,7 @@ public class AdvancedActivity extends Activity {
         setContentView(R.layout.activity_advance);
 
         mCodeView = findViewById(R.id.code_content);
+        // mCodeView.setTheme(CodeViewTheme.ANDROIDSTUDIO).fillColor();
 
         mCurActivityName = getIntent().getStringExtra(EXTRA_CUR_ACTIVITY_NAME);
         Log.i(TAG, "mCurActivityName=" + mCurActivityName);
@@ -70,11 +88,17 @@ public class AdvancedActivity extends Activity {
     }
 
     private static class CopyUnzipTask extends AsyncTask<Void, Void, Boolean> {
-        /** Weak reference. */
+        /**
+         * Weak reference.
+         */
         private WeakReference<AdvancedActivity> mActivityWeakReference;
-        /** App sdcard path. */
+        /**
+         * App sdcard path.
+         */
         private String mAppSdcardPath = AdvancedConstants.CODE_SDCARD_PATH;
-        /** Code zip name. */
+        /**
+         * Code zip name.
+         */
         private String mCodeZipName = AdvancedConstants.CODE_ASSET_PATH;
 
         /**
@@ -143,7 +167,9 @@ public class AdvancedActivity extends Activity {
      * Load code task.
      */
     public static class LoadCodeTask extends AsyncTask<File, Void, String> {
-        /** Weak reference. */
+        /**
+         * Weak reference.
+         */
         private WeakReference<AdvancedActivity> mActivityWeakReference;
 
         /**
@@ -167,7 +193,7 @@ public class AdvancedActivity extends Activity {
         protected void onPostExecute(String ret) {
             super.onPostExecute(ret);
             if (!TextUtils.isEmpty(ret) && mActivityWeakReference.get() != null) {
-                mActivityWeakReference.get().mCodeView.setText(ret);
+                mActivityWeakReference.get().mCodeView.showCode(ret);
             } else {
                 Log.i(TAG, "Empty file content");
             }
